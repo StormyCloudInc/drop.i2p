9565a2f9b9a7508ee5b80ea79579959e564d52bd
stormycloud <stormycloud@mail.i2p>
2025-06-21 14:58:32 -0400

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-# wsgi.py
-# This file is the entry point for the Gunicorn WSGI server.
-# It imports the 'app' object from your main application file.
-
-from app import app
-
-if __name__ == "__main__":
-    app.run()
